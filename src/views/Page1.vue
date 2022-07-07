78a91bcc96f9259c8b7e77cbc4fb2e65f0e3c6f9
Sumeet Patel <sumeet.patel@eroam.com>
2022-07-07 12:42:31 +0530

    updated code for development

---
+++ src/views/Page1.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="my-5">
+      <DetailCard v-for="i in 2" :key="i" :class="{'right-align': i%2 == 0}" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import DetailCard from "@/components/DetailCard.vue"
+@Component({
+  components: {
+    DetailCard
+  },
+})
+export default class Home extends Vue {}
+</script>
